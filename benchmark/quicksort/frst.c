cd6c54257abc3c911f5590fcf7e872dd562bec87
vnzstc <vnzstc@protonmail.com>
2020-11-01 13:40:30 +0100

    adding atmega, leon3 version of the quicksort

--- benchmark/quicksort/frst.c
+++ benchmark/quicksort/frst.c
@@ -1,19 +1,9 @@
-/* 
- * This is the implementation of sorting algorithm QuickSort:
- * Input:  Monodimensional array of integers 
- * Output: Monodimensional array of sorted integers 
- * Characteristics: In place, uses static memory allocation
- */
-
 #include <stdint.h>
 #include <values.h>
 
-typedef int8_t TARGET_TYPE;
-typedef int8_t TARGET_INDEX;
+typedef float TARGET_TYPE;
+typedef long TARGET_INDEX;
 
-//void prototype(int8_t size, float a[size]);
-
-/* As suggested by the name, this function changes the position of two elements which are at position index_1 and index_2 */ 
 void swap(TARGET_INDEX index_1, TARGET_INDEX index_2)
 {
 
@@ -23,85 +13,63 @@ void swap(TARGET_INDEX index_1, TARGET_INDEX index_2)
 }
 
 
-/* 
- * This is the core function of QuickSort, it initializes two pointers (inf and sup) and moves them to an element that is > pivot
- * (in case of inf), <= pivot (in case of sup). At the end, exchanges the elements indicated by pointers.
- * It returns the position of pivot after partition procedure.
- */
-
-TARGET_TYPE partition(TARGET_INDEX init, TARGET_INDEX end)
+TARGET_INDEX partition(TARGET_INDEX low, TARGET_INDEX high)
 {
-	TARGET_TYPE pivot = a[init];
-	TARGET_INDEX sup = end;
-	TARGET_INDEX inf = init;
+	TARGET_INDEX idx = low - 1;
+	TARGET_INDEX j;
 
-	while(1)
+	for(j = low; j <= high-1; j++)
 	{
-		while(inf <= end && a[inf] <= pivot)
-			++inf;
-
-		while(a[sup] > pivot)
-			--sup;
-
-		//printf("%d, %d", inf, sup);
-		if(inf < sup+1)
-			swap(inf, sup);
-		else
-			break;
-
+		if(a[j] < a[high])
+		{
+			idx++;
+			swap(idx, j);
+		}
 	}
 
-	swap(init, sup);
-	return sup;
-
+	swap(idx+1, high);
+	return idx+1;
 }
 
+void quicksort_0(TARGET_INDEX low, TARGET_INDEX high)
+{
+	enum{ssize = 130};
+	TARGET_INDEX stack[ssize];
 
-/* 
- * The recursive behaviour of quicksort is implemented through a stack. In each frame are stored 
- * the initial index and the final index of the array. This serves to call partition on subarrays.
- */
+	TARGET_INDEX top = -1;
+	TARGET_INDEX p;
+
+	stack[++top] = low;
+	stack[++top] = high;
+
+	while(top >= 0)
+	{
+		high = stack[top--];
+		low = stack[top--];
+
+		p = partition(low, high);
+
+		if(p - 1 > low)
+		{
+			stack[++top] = low;
+			stack[++top] = p -1;
+		}
+
+		if(p + 1 < high)
+		{
+			stack[++top] = p + 1;
+			stack[++top] = high;
+		}
+
+	}
+}
 
 void quicksort(TARGET_INDEX size, TARGET_TYPE a[size])
 {
-	TARGET_TYPE stack[size][2];
-	TARGET_INDEX stack_size = -1;
-
-	/* This pointer always indicates the head of the stack */
-	TARGET_TYPE *top = stack[0];
-
-	TARGET_TYPE pivot_position = 0;
-	TARGET_TYPE base = 0;
-
-	stack[++stack_size][0] = size;
-	stack[stack_size][1] = a[size];
-
-	while(stack_size >= 0)
-	{
-		if(top[0] < top[1])
-		{	
-			pivot_position = partition(top[0], top[1]);
-			
-			base  = top[0];
-
-			stack[stack_size][0] = pivot_position+1;
-			//stack[size][1] = top[1];
-			++stack_size;
-			stack[stack_size][0] = base;
-			stack[stack_size][1] = pivot_position-1;
-
-		}else{
-
-			stack_size--;
-		}
-
-
-		top = stack[stack_size];
-	}
+	quicksort_0(0, size-1);
 }
-
 
 void main()
 {
- 	quicksort(size, a);
+	quicksort(size, a);
 }
--- benchmark/quicksort/frst_old.c
+++ benchmark/quicksort/frst_old.c
@@ -1,9 +1,19 @@
+/* 
+ * This is the implementation of sorting algorithm QuickSort:
+ * Input:  Monodimensional array of integers 
+ * Output: Monodimensional array of sorted integers 
+ * Characteristics: In place, uses static memory allocation
+ */
+
 #include <stdint.h>
 #include <values.h>
 
-typedef float TARGET_TYPE;
-typedef long TARGET_INDEX;
+typedef int8_t TARGET_TYPE;
+typedef int8_t TARGET_INDEX;
 
+//void prototype(int8_t size, float a[size]);
+
+/* As suggested by the name, this function changes the position of two elements which are at position index_1 and index_2 */ 
 void swap(TARGET_INDEX index_1, TARGET_INDEX index_2)
 {
 
@@ -13,63 +23,85 @@ void swap(TARGET_INDEX index_1, TARGET_INDEX index_2)
 }
 
 
-TARGET_INDEX partition(TARGET_INDEX low, TARGET_INDEX high)
-{
-	TARGET_INDEX idx = low - 1;
-	TARGET_INDEX j;
+/* 
+ * This is the core function of QuickSort, it initializes two pointers (inf and sup) and moves them to an element that is > pivot
+ * (in case of inf), <= pivot (in case of sup). At the end, exchanges the elements indicated by pointers.
+ * It returns the position of pivot after partition procedure.
+ */
 
-	for(j = low; j <= high-1; j++)
+TARGET_TYPE partition(TARGET_INDEX init, TARGET_INDEX end)
+{
+	TARGET_TYPE pivot = a[init];
+	TARGET_INDEX sup = end;
+	TARGET_INDEX inf = init;
+
+	while(1)
 	{
-		if(a[j] < a[high])
-		{
-			idx++;
-			swap(idx, j);
-		}
+		while(inf <= end && a[inf] <= pivot)
+			++inf;
+
+		while(a[sup] > pivot)
+			--sup;
+
+		//printf("%d, %d", inf, sup);
+		if(inf < sup+1)
+			swap(inf, sup);
+		else
+			break;
+
 	}
 
-	swap(idx+1, high);
-	return idx+1;
+	swap(init, sup);
+	return sup;
+
 }
 
-void quicksort_0(TARGET_INDEX low, TARGET_INDEX high)
-{
-	enum{ssize = 130};
-	TARGET_INDEX stack[ssize];
 
-	TARGET_INDEX top = -1;
-	TARGET_INDEX p;
-
-	stack[++top] = low;
-	stack[++top] = high;
-
-	while(top >= 0)
-	{
-		high = stack[top--];
-		low = stack[top--];
-
-		p = partition(low, high);
-
-		if(p - 1 > low)
-		{
-			stack[++top] = low;
-			stack[++top] = p -1;
-		}
-
-		if(p + 1 < high)
-		{
-			stack[++top] = p + 1;
-			stack[++top] = high;
-		}
-
-	}
-}
+/* 
+ * The recursive behaviour of quicksort is implemented through a stack. In each frame are stored 
+ * the initial index and the final index of the array. This serves to call partition on subarrays.
+ */
 
 void quicksort(TARGET_INDEX size, TARGET_TYPE a[size])
 {
-	quicksort_0(0, size-1);
+	TARGET_TYPE stack[size][2];
+	TARGET_INDEX stack_size = -1;
+
+	/* This pointer always indicates the head of the stack */
+	TARGET_TYPE *top = stack[0];
+
+	TARGET_TYPE pivot_position = 0;
+	TARGET_TYPE base = 0;
+
+	stack[++stack_size][0] = size;
+	stack[stack_size][1] = a[size];
+
+	while(stack_size >= 0)
+	{
+		if(top[0] < top[1])
+		{	
+			pivot_position = partition(top[0], top[1]);
+			
+			base  = top[0];
+
+			stack[stack_size][0] = pivot_position+1;
+			//stack[size][1] = top[1];
+			++stack_size;
+			stack[stack_size][0] = base;
+			stack[stack_size][1] = pivot_position-1;
+
+		}else{
+
+			stack_size--;
+		}
+
+
+		top = stack[stack_size];
+	}
 }
+
 
 void main()
 {
-	quicksort(size, a);
+ 	quicksort(size, a);
 }
